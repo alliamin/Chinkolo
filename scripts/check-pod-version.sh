0400a3f61d7e22380b73af2669fc203b1b8ce4b9
aliamin <aamin@instabug.com>
2018-02-02 15:33:44 +0200

    Adds more styling

--- scripts/check-pod-version.sh
+++ scripts/check-pod-version.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# Get new version
 version_line=$(ls -l | grep -i '^  s.version' Moya.podspec)
 version=$(echo $version_line | cut -d \= -f 2)
 version=$(echo $version | tr -d \" | tr -d ' ')
 
-#Update Readme(s)
+# Update Readme(s)
 ## Update pod 'Moya'
 sed -i -e "s#pod 'Moya'.*#pod 'Moya' '~> ${version}'#" *.md
 ## Update pod 'Moya/RxSwift'
 sed -i -e "s#pod 'Moya/RxSwift'.*#pod 'Moya/RxSwift' '~> ${version}'#" *.md
 ## Update pod 'Moya/ReactiveSwift'
 sed -i -e "s#pod 'Moya/ReactiveSwift'.*#pod 'Moya/ReactiveSwift' '~> ${version}'#" *.md
+
+# Push changes
+## WIP
