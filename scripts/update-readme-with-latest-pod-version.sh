2871799637f983d02df0a1ed493699463eac620e
CircleCI <ali_amin@live.com>
2018-02-09 20:57:39 +0200

    modification

--- scripts/update-readme-with-latest-pod-version.sh
+++ scripts/update-readme-with-latest-pod-version.sh
@@ -19,5 +19,5 @@ git config user.email "ali_amin@live.com"
 git config user.name "cisclub"
 git clone --depth 1 https://${GITHUB_PERSONAL_TOKEN}@github.com/cisclub/Chinkolo.git
 cd Chinkolo/
-git commit -m "Update 'Pod install' in Readme"
+git commit -m --allow-empty "Update 'Pod install' in Readme"
 git push origin master
