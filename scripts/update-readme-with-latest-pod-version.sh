cea5b22d6c0a655fa5acfacb30c0dfcfb071c126
CircleCI <ali_amin@live.com>
2018-02-10 02:37:56 +0200

    modification

--- scripts/update-readme-with-latest-pod-version.sh
+++ scripts/update-readme-with-latest-pod-version.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-git config credential.helper 'cache --timeout=300'
-git config user.email "ali_amin@live.com"
-git config user.name "cisclub"
-git clone --depth 1 https://${GITHUB_PERSONAL_TOKEN}@github.com/cisclub/Chinkolo.git
-git checkout origin master
-
 # Get new version
 version_line=$(ls -l | grep -i '^  s.version' Moya.podspec)
 version=$(echo $version_line | cut -d \= -f 2)
@@ -19,9 +13,13 @@ sed -i -e "s#pod 'Moya/RxSwift'.*#pod 'Moya/RxSwift' '~> ${version}'#" *.md
 ## Update pod 'Moya/ReactiveSwift'
 sed -i -e "s#pod 'Moya/ReactiveSwift'.*#pod 'Moya/ReactiveSwift' '~> ${version}'#" *.md
 
+
+git config credential.helper 'cache --timeout=300'
+git config user.email "ali_amin@live.com"
+git config user.name "cisclub"
 cd Chinkolo/
 echo "in Chinkolo"
 git commit -m "Update 'Pod install' in Readme"
 echo "commit done"
-git push origin master
+git push --verbose origin master
 echo "pushing to master"
