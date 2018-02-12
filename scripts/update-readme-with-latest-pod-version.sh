acbf58edbc7086d3a0f066e980ad5303584221d1
CircleCI <ali_amin@live.com>
2018-02-12 20:34:17 +0200

    modification

--- scripts/update-readme-with-latest-pod-version.sh
+++ scripts/update-readme-with-latest-pod-version.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
-# Get new version
-version_line=$(ls -l | grep -i '^  s.version' Moya.podspec)
-version=$(echo $version_line | cut -d \= -f 2)
-version=$(echo $version | tr -d \" | tr -d ' ')
 
-# Update Readme(s)
-## Update pod 'Moya'
-sed -i -e "s#pod 'Moya'.*#pod 'Moya' '~> ${version}'#" *.md
-## Update pod 'Moya/RxSwift'
-sed -i -e "s#pod 'Moya/RxSwift'.*#pod 'Moya/RxSwift' '~> ${version}'#" *.md
-## Update pod 'Moya/ReactiveSwift'
-sed -i -e "s#pod 'Moya/ReactiveSwift'.*#pod 'Moya/ReactiveSwift' '~> ${version}'#" *.md
-
-
-#Push changes if last commit is not "Update Readme"
+#Update and Push changes if last commit is not "Update Readme"
 last_sha=$(git rev-parse HEAD)
 commit_message=$(git log -1 --pretty=%B $last_sha)
+echo "cm -> ${commit_message}"
+
+exit 1
 if [ commit_message != "Update Readme" ]; then
+	# Get new version
+	version_line=$(ls -l | grep -i '^  s.version' Moya.podspec)
+	version=$(echo $version_line | cut -d \= -f 2)
+	version=$(echo $version | tr -d \" | tr -d ' ')
+
+	# Update Readme(s)
+	## Update pod 'Moya'
+	sed -i -e "s#pod 'Moya'.*#pod 'Moya' '~> ${version}'#" *.md
+	## Update pod 'Moya/RxSwift'
+	sed -i -e "s#pod 'Moya/RxSwift'.*#pod 'Moya/RxSwift' '~> ${version}'#" *.md
+	## Update pod 'Moya/ReactiveSwift'
+	sed -i -e "s#pod 'Moya/ReactiveSwift'.*#pod 'Moya/ReactiveSwift' '~> ${version}'#" *.md
+
 	echo "Pushing chnages to Readme(s)."
 	git config credential.helper 'cache --timeout=300'
 	git config user.email "ali_amin@live.com"
